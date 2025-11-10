5297f18d8a39cba4be9cc6161b8e1e16a9194634
SandmanMaybeTe23 <billy.burlin@elev.ga.ntig.se>
2025-11-10 11:14:16 +0100

    hurry i win

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
-playerName=prompt("")
+playerName=prompt()
 
+
+
+
+
+
+const playButton = document.querySelector("#play-button")
 const playerNameChanger= document.querySelector("#playerName")
 const playerHpChanger = document.querySelector("#playerHp")
 const enemyHpChanger = document.querySelector("#enemyHp")
 
-playerNameChanger.textContent=+playerName
+
+playerNameChanger.textContent=playerName
+
 
 let playerHp = 100
-let enemyHp = 100
+
+
+
+const combatLogElement = document.querySelector("#combat-log")
+
+function log(msg,type){
+    const li = document.createElement("li")
+
+    if(type){
+        li.classList.add(type)
+    }
+
+    li.textContent = msg
+    combatLogElement.appendChild(li)
+
+    if(combatLogElement.childNodes.length>10){
+        combatLogElement.removeChild(combatLogElement.firstChild)
+    }
+}
+
+
+
+const playerAttack = [
+    "are just better and you deal :" ,
+   //nope 
+]
+
+
+
+const enemy =  {
+    "name": "goblin",
+    "hp": 40,
+    "attack":0.5
+}
+
+
+
 
 function rollDice() {
     return Math.ceil(Math.random() * 6)
@@ -20,29 +64,38 @@ function fightRound(){
     const enemyRoll = rollDice()
 
     if (playerRoll > enemyRoll) {
-        console.log("player hit enemy")
+        log( " have hit the enemy because your just better", "player")
         const playerHit = playerRoll - enemyRoll
-        enemyHp -= playerHit
-        console.log(enemyHp)
+        enemy.hp -= playerHit
+        log(playerHit)
     }
 
 
     else if (playerRoll < enemyRoll) {
-        console.log("enemy has skill")
+        log("enemy has skill", "enemy")
         const enemyHit = enemyRoll - playerRoll
-        playerHp -= enemyHit
-        console.log(playerHp)
+        playerHp -= enemyHit*enemy.attack
+        log(enemyHit)
     }
 
     else {
-        console.log("you both suck")
+        log("you both suck","pain")
     }
 
     playerHpChanger.textContent= playerHp
-    enemyHpChanger.textContent=enemyHp
+    enemyHpChanger.textContent= enemy.hp
+
+    if (playerHp < 1 || enemy.hp < 1){
+        playButton.disabled = true
+
+    }
+    else if (playerHp < 30)
+        playerHpChanger.classList.add("hp-low")
+
+    playerHpChanger.textContent = playerHp < 1 ? 0 : playerHp
+    enemyHpChanger.textContent = playerHp < 1 ? 0 : enemy.hp
 }
 
 
-const playButton = document.querySelector("#play-button")
 
 playButton.addEventListener("click", fightRound)
