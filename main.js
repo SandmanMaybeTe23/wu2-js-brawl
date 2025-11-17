9b125ba9be95ac46f87b43de5c3b244040ac7d32
SandmanMaybeTe23 <billy.burlin@elev.ga.ntig.se>
2025-11-17 11:18:19 +0100

    bugs and alot of them

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-playerName=prompt()
-
-
-
-
+playerName="addwa"
 
 
 const playButton = document.querySelector("#play-button")
+const stopButton = document.querySelector("#stop-button")
 const playerNameChanger= document.querySelector("#playerName")
 const playerHpChanger = document.querySelector("#playerHp")
+const enemyNameChanger= document.querySelector("#enemyName")
 const enemyHpChanger = document.querySelector("#enemyHp")
 
 
 playerNameChanger.textContent=playerName
 
-
 let playerHp = 100
-
 
 
 const combatLogElement = document.querySelector("#combat-log")
@@ -43,15 +39,28 @@ const playerAttack = [
 ]
 
 
+function spawnEnemy() {
+    const enemyNames = ["Goblin", "Orc", "Troll", "SANS", "ECIN"]
+    const name = enemyNames[Math.floor(Math.random() * enemyNames.length)]
+    const attack = Math.ceil(Math.random() * 2)
+    const hp = Math.floor(Math.random() * 50 + 30)/attack
+    
+    return new Enemy(name, hp, attack)
+}
 
-const enemy =  {
-    "name": "goblin",
-    "hp": 40,
-    "attack":0.5
+class Enemy {
+    constructor(name, hp,attack) {
+        this.name = name
+        this.hp = hp
+        this.attack= attack
+    }
 }
 
 
+const enemy = spawnEnemy()
 
+
+enemyNameChanger.textContent=enemy.name
 
 function rollDice() {
     return Math.ceil(Math.random() * 6)
@@ -82,13 +91,12 @@ function fightRound(){
         log("you both suck","pain")
     }
 
-    playerHpChanger.textContent= playerHp
-    enemyHpChanger.textContent= enemy.hp
+
 
     if (playerHp < 1 || enemy.hp < 1){
         playButton.disabled = true
-
     }
+
     else if (playerHp < 30)
         playerHpChanger.classList.add("hp-low")
 
@@ -97,5 +105,49 @@ function fightRound(){
 }
 
 
+let last = 0
 
-playButton.addEventListener("click", fightRound)
+function gameLoop(timestamp) {
+    if (timestamp >= last + 500) {
+        fightRound()
+        last = timestamp;
+    }
+    round = window.requestAnimationFrame(gameLoop)
+
+ if (playerHp < 1) {
+        playButton.disabled = true
+        log(`you have deep skill issue ,and ${enemy.name} has won ???`, "enemy")
+        window.cancelAnimationFrame(round)
+    } else {
+        round = window.requestAnimationFrame(gameLoop)
+    }
+
+    if (enemy.hp < 1) {
+        log(` you have slain ${enemy.name}! wow im so impress :/` , "player")
+        enemy = spawnEnemy()
+        log(`well you need more to kill her you have ${enemy.name} go kill him yea`, "enemy")
+        const heal = Math.floor(Math.random() * 20 + 10)
+        log(`you scream MEDIC! and the medic heal you ${heal} hp! yup so true`, "player")
+        playerHp += heal
+    } else if (playerHp < 30) {
+        playerHpElement.classList.add("low-hp")
+    }
+
+}
+
+
+
+
+
+function stop() {
+    console.log("stop")
+    window.cancelAnimationFrame(round)
+}
+
+
+playerHpChanger.textContent= playerHp
+enemyHpChanger.textContent= enemy.hp
+playButton.addEventListener("click", gameLoop)
+stopButton.addEventListener("click", stop)
+
+
